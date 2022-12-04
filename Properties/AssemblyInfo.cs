fd395df3fc77e61c0ff79d0bb62657b33898687f
michael-fa <michixx.bat@gmail.com>
2022-12-04 17:02:42 +0100

    Renamed plugin to FileEx

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("DCAMX-IOEX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DCAMX-FileEx")]
+[assembly: AssemblyDescription("File/Folder native extension for DCAMX scripts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DCAMX-IOEX")]
+[assembly: AssemblyProduct("DCAMX-FileEx")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
